f568542a9ee2a56866a930e73c0fb38915eba0cd
DJMyles <daniel@mylesapart.io>
2018-12-31 00:37:11 +1100

    some node stuff and a new view partial

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
+var postcss = require('gulp-postcss');
 var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('default', function(){
   return gulp.src('scss/style.scss')
     .pipe(sass()) // Using gulp-sass
+    .pipe(postcss([ autoprefixer() ]))
     .pipe(gulp.dest('css/'))
 });
